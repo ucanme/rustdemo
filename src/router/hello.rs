e9a47fe1a71b79d3b6c00e833184baa41d983e2c
Your Name <you@example.com>
2025-01-01 22:42:42 +0800

    123

--- src/router/hello.rs
+++ src/router/hello.rs
@@ -1,6 +1,14 @@
-use actix_web::{get, HttpResponse, Responder};
+use std::process::id;
+use actix_web::{get, web, HttpResponse, Responder};
+use crate::model::post;
+use sea_orm::{ConnectionTrait, EntityTrait};
 
 #[get("/")]
-pub async fn hello() -> impl Responder {
+pub async fn hello(db:web::Data<sea_orm::DatabaseConnection>) -> impl Responder {
+    let posts: Vec<post::Model> = post::Entity::find_by_id(1).all(db.get_ref()).await.unwrap();
+    println!("表中的所有帖子:");
+    for post in posts {
+        println!("id: {}, title: {}", post.id, post.content);
+    }
     HttpResponse::Ok().body("Hello world!")
 }
